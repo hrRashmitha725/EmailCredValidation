fdff5202d85f0b01ea70415e5d4a90ea05825f4b
hrRashmitha257 <rashmithahr777@gmail.com>
2022-12-02 16:09:02 +0530

    framewor added

---
+++ EmailCredValidation/EmailCredValidation/EmailCredValidation.h
@@ -0,0 +1,18 @@
+//
+//  EmailCredValidation.h
+//  EmailCredValidation
+//
+//  Created by Softsuave on 02/12/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmailCredValidation.
+FOUNDATION_EXPORT double EmailCredValidationVersionNumber;
+
+//! Project version string for EmailCredValidation.
+FOUNDATION_EXPORT const unsigned char EmailCredValidationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmailCredValidation/PublicHeader.h>
+
+
